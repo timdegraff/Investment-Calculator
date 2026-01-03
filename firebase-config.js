f84b7d22a33f7826fa6a006681d7639f9a7db0fb
degraff.tim <degraff.tim@gmail.com>
2026-01-03 03:30:58 +0000

    fix

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyAqvvl6O3-2TLK4-j6ei1WU5SuaMY3HnO4",
   authDomain: "investment-calculator-931f2.firebaseapp.com",
@@ -7,3 +12,10 @@ export const firebaseConfig = {
   appId: "1:943895110805:web:ba6e1e6177ae31d7af279c",
   measurementId: "G-F0CRPKLS7G"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Export the instances
+export { app, auth };
